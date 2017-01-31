ca8334694365069664844b79093863fd0d0bf467
franklang <lang.frank79@gmail.com>
2017-01-31 23:15:14 +0100

    Prefix migration from .fs- to .nkd- + CSS/JS code optimization. Now works as a plugin.

---
+++ js/nkd-accordion.js
@@ -0,0 +1,53 @@
+;(function ($, window, undefined){
+  $.fn.nkdAccordion = function(){
+
+    // Set clicked link sublevel state (open/closed)
+    function setSubLevelState(element,state){
+      element
+        .parent('.js-nkd-item')
+        .attr('data-state', state);
+    }
+
+    function onLinkClick(){
+      // Get current nav level of clicked link
+      var getLinkNavLevel = $(this)
+        .closest('.js-nkd-level')
+        .attr('data-level');
+
+      // Get clicked link sublevel state (open/closed)
+      var getSubLevelState = $(this)
+        .parent('.js-nkd-item')
+        .attr('data-state');
+
+      // Close children sublevels
+      var closeChildren = $(this)
+        .parent('.js-nkd-item')
+        .find('[data-state="open"]')
+        .each(function(){
+          $(this)
+            .attr('data-state', 'closed');
+        });
+
+      if (getSubLevelState == 'closed'){
+        if (getLinkNavLevel == 0){
+          $(this)
+            .parent('.js-nkd-item')
+            .siblings('[data-state="open"]')
+            .attr('data-state', 'closed');
+
+          closeChildren
+        }
+        setSubLevelState($(this),'open');
+      }
+
+      else if (getSubLevelState == 'open'){
+        setSubLevelState($(this),'closed');
+
+        closeChildren
+      }
+    }
+
+    $('.js-nkd-link').on('click', onLinkClick);
+
+  };
+}(jQuery, window));
